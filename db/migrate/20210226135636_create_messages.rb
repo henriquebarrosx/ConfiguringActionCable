ae43621f8878629f3ff392ef6022c494c837827a
Henriquebarros <henriquexavierb@gmail.com>
2021-02-26 11:04:11 -0300

    creating the room and message migrations

---
+++ db/migrate/20210226135636_create_messages.rb
@@ -0,0 +1,12 @@
+class CreateMessages < ActiveRecord::Migration[6.1]
+  def change
+    create_table :messages do |t|
+      t.text :data
+      t.string :type
+      t.binary :file
+      t.references :room, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
