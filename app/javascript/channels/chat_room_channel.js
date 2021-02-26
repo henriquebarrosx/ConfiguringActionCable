6a8600cf38fb56d63dda661aa1413cf790a88c34
Henriquebarros <henriquexavierb@gmail.com>
2021-02-26 10:55:49 -0300

    making dynamic rooms

--- app/javascript/channels/chat_room_channel.js
+++ app/javascript/channels/chat_room_channel.js
@@ -1,18 +1,21 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create("ChatRoomChannel", {
-  connected() {
-    // Called when the subscription is ready for use on the server
-    console.log('Channel Connected!!!');
-  },
+document.addEventListener('turbolinks:load', () => {
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-    console.log('Channel Disconnected!!!');
-  },
-
-  received(data) {
-    // Called when there's incoming data on the websocket for this channel
-    console.log('Message: ', data);
-  }
-});
+  consumer.subscriptions.create({ channel: "ChatRoomChannel", room_id: 1 }, {
+    connected() {
+      // Called when the subscription is ready for use on the server
+      console.log('Channel Connected!!!');
+    },
+  
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+      console.log('Channel Disconnected!!!');
+    },
+ 
+    received(data) {
+      // Called when there's incoming data on the websocket for this channel
+      console.log('Message: ', data);
+    }
+  });
+})
